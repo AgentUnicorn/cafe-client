a640df65ba8aae602d8ef5b129e8e311ea21544f
Lê Công Nam <namlem4u@gmail.com>
2021-12-23 09:41:43 +0700

    Add Footer Component in app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
 import Banner from './Components/Banner.js'
+import Banner from './Components/Footer.js'
 import './App.css';
 
 function App() {
   return (
     <div>
       <Banner />
+      <Footer/>
     </div>
   );
 }
